c386db30003389c4d764bf7b087cb07846d4c91e
KRYPTON0078 <KRYPTON0078@users.noreply.github.com>
2026-07-24 11:01:10 +0800

    Centralize supported Agent protocol versions.

---
+++ hes-common/src/main/java/com/hes/common/protocol/ProtocolVersions.java
@@ -0,0 +1,14 @@
+﻿package com.hes.common.protocol;
+
+import java.util.Set;
+
+public final class ProtocolVersions {
+    public static final String V1 = "1.0";
+    public static final Set<String> SUPPORTED = Set.of(V1);
+
+    private ProtocolVersions() {}
+
+    public static boolean isSupported(String version) {
+        return version != null && SUPPORTED.contains(version);
+    }
+}
