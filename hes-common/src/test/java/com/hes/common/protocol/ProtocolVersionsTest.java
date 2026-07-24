15917cb7335b5ad9a01ef01401eba0520c5a2f4e
KRYPTON0078 <KRYPTON0078@users.noreply.github.com>
2026-07-24 11:01:10 +0800

    Verify protocol version support checks.

---
+++ hes-common/src/test/java/com/hes/common/protocol/ProtocolVersionsTest.java
@@ -0,0 +1,16 @@
+﻿package com.hes.common.protocol;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProtocolVersionsTest {
+    @Test
+    void acceptsV1() {
+        assertTrue(ProtocolVersions.isSupported("1.0"));
+    }
+
+    @Test
+    void rejectsUnknown() {
+        assertFalse(ProtocolVersions.isSupported("9.9"));
+    }
+}
